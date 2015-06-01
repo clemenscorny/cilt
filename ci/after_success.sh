4501ef10348607797cb24ef0d3ade06391307da7
Clemens Korner <clemens.corny@gmail.com>
2015-06-01 13:41:52 +0200

    Remove Coveralls support

--- ci/after_success.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $CXX = "g++" ]; then
-    make coverage
-    coveralls-lcov coverage.info.cleaned
-#    coveralls
-fi
--- ci/before_script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $CXX = "g++" ]; then
-    cmake -DENABLE_TESTING_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
-else
-    cmake -DCMAKE_BUILD_TYPE=Debug ..
-fi
